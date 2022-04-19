6257c8b11658efac91080aee98503c0de59de2e4
jdwv <86278310+jdwv@users.noreply.github.com>
2022-04-19 20:41:40 +1000

    Create linux-setup.sh
    Initial commit

---
+++ linux-setup.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/bash
+
+# Super bad it, don't do this. I wrote this file, therefore I trust it.
+# wget -O - https://thisfile.sh | bash
+
+# Install apps
+ApplicationList=(
+    "zsh"
+    "tmux"
+    "vim"
+    "git"
+    "curl"
+)
+echo "Updating repositories before installing apps"
+sudo apt update
+for app in ${ApplicationList[@]}; do 
+    echo "Checking $app"
+    dpkg -s "$app" &> /dev/null
+    if [[ $? -ne 0 ]]; then
+        echo "${app} not installed"
+        sudo apt install -y $app
+    else
+        echo "${app} already installed - skipping"
+    fi
+done
+
+# Change shell
+sudo chsh -s $(which zsh) $(whoami)
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Backup config files in $HOME
+configFileList=(
+    ".zshrc"
+    ".p10k.zsh"    
+)
+for configFile in ${configFileList[@]}; do 
+    configFilePath="${HOME}/${configFile}"
+    if [[ -f "${configFilePath}" ]]; then 
+        echo "${configFilePath} exists - backing up"
+        backupTimeStamp=`date +"%Y%m%d-%H%M%S"`
+        newConfigFileName="${configFilePath}.${backupTimeStamp}.bak"
+        mv configFilePath newConfigFileName
+    else
+        echo "${configFilePath} does not exist - skipping backup"
+    fi
+done
+
+
+# Download config files from git repo
+configDownloadList=(
+    #".zshrc"
+    #".p10k.zsh"    
+)
+for cloudFile in ${configDownloadList[@]}; do 
+    cd $HOME
+    curl -O $cloudFile
+done
