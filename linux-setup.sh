b0904d57fe55e885ccd1f41229cbe00eea817bee
jdwv <86278310+jdwv@users.noreply.github.com>
2022-04-23 17:01:43 +1000

    Update linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -30,10 +30,19 @@ done
 sudo chsh -s $(which zsh) $(whoami)
 
 # Install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+if [[ ! -d "${HOME}/.oh-my-zsh" ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "ohmyzsh already installed - skipping"
+fi
 
 # Clone powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if [[ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+else
+    echo "powerlevel10k already installed - skipping"
+fi
+
 
 # Backup config files in $HOME
 configFileList=(
