88300a55c60f03483a31132ae59e758373abec9d
jdwv <86278310+jdwv@users.noreply.github.com>
2023-07-26 19:01:00 +1000

    Removed redundant self_updater

--- silverblue-apps.sh
+++ silverblue-apps.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Function to update the script from Git
-self_update() {
-    cd $1 || exit
-    #git pull origin main
-}
-
 # Function to update Flatpak apps
 update_flatpak_apps() {
 	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -16,8 +10,7 @@ update_flatpak_apps() {
 
     while IFS= read -r flatpak_app; do
         flatpak install -y --noninteractive flathub "$flatpak_app"
-    #done < "$CONFIG_DIR/flatpak_apps.txt"
-    done < $(wget -O - https://raw.githubusercontent.com/jdwv/linux-quick-setup/main/flatpak_apps.txt)
+    done < <(wget -O - https://raw.githubusercontent.com/jdwv/linux-quick-setup/main/flatpak_apps.txt)
 
 }
 
@@ -25,18 +18,8 @@ update_flatpak_apps() {
 update_rpm_ostree_apps() {
     while IFS= read -r rpm_ostree_app; do
         rpm-ostree install -y "$rpm_ostree_app"
-    #done < "$CONFIG_DIR/rpm-ostree_apps.txt"
-    done < $(wget -O - https://raw.githubusercontent.com/jdwv/linux-quick-setup/main/rpm-ostree_apps.txt)
+    done < <(wget -O - https://raw.githubusercontent.com/jdwv/linux-quick-setup/main/rpm-ostree_apps.txt)
 }
-
-# Set CONFIG_DIR to the directory where the script is located
-
-MY_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
-MY_PATH="$(cd -- "$MY_PATH" && pwd)"
-CONFIG_DIR=$MY_PATH
-
-# Perform self-update from Git
-#self_update $CONFIG_DIR
 
 # Update Flatpak apps
 update_flatpak_apps
