1de1b65244b8a3ebc297c480f8058f538daba067
jdwv <github@jdwv.me>
2023-10-29 19:03:50 +1100

    Update linux-setup.sh
    Change $SUDO_USER as it appears to be null in some cases.

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
-
 # Set home dir to sudo user
-sudo_user_home=$(eval echo "~$SUDO_USER")
-cd $sudo_user_home
+username=$(whoami)
+user_home=$(getent passwd "$username" | cut -d: -f6)
+
+cd $user_home
 
 # Super bad it, don't do this. I wrote this file, therefore I trust it.
 # wget -O - https://thisfile.sh | bash
@@ -185,15 +186,14 @@ if [[ $? -eq 0 ]]; then # Check zsh installed
     
     if [ -z "$ZSH" ]; then
         # Set ZSH var if running from bash
-        echo "zsh running from bash - setting ZSH to ${sudo_user_home}/.oh-my-zsh"
-        ZSH="${sudo_user_home}/.oh-my-zsh"
+        echo "zsh running from bash - setting ZSH to ${user_home}/.oh-my-zsh"
+        ZSH="${user_home}/.oh-my-zsh"
     fi
 
     ################
     # Change shell #
     ################
     # Define the username and desired shell
-    username=$SUDO_USER
     desired_shell="zsh"
     # Get the correct path for the desired shell
     shell_path=$(which "$desired_shell")
@@ -205,7 +205,7 @@ if [[ $? -eq 0 ]]; then # Check zsh installed
     if [ -d "$ZSH" ]; then
     	echo "$ZSH is a directory and exists"
 	    # Check if ohmyzsh repo exists in home directory
-	    ohmyzsh_remote_url=$(sudo -u $SUDO_USER git -C ~/.oh-my-zsh remote get-url origin)
+	    ohmyzsh_remote_url=$(sudo -u $username git -C ~/.oh-my-zsh remote get-url origin)
 	    official_remote_url="https://github.com/ohmyzsh/ohmyzsh.git"
 	if [[ "$ohmyzsh_remote_url" == "$official_remote_url" ]]; then
 	    echo "Oh My Zsh - already installed - updating"
@@ -216,78 +216,78 @@ if [[ $? -eq 0 ]]; then # Check zsh installed
 	fi
     else
         echo "Oh My Zsh - new install"
-        sudo -u $SUDO_USER sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null
+        sudo -u $username sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null
     fi
 
     #######################
     # Clone powerlevel10k #
     #######################
-    TARGET_DIR="${ZSH_CUSTOM:-$sudo_user_home/.oh-my-zsh/custom}/themes/powerlevel10k"
+    TARGET_DIR="${ZSH_CUSTOM:-$user_home/.oh-my-zsh/custom}/themes/powerlevel10k"
     REPO="https://github.com/romkatv/powerlevel10k.git"
     BRANCH=$(git ls-remote --heads $REPO | grep -q 'refs/heads/main' && echo 'main' || (git ls-remote --heads $REPO | grep -q 'refs/heads/master' && echo 'master') || echo null)
 
     if [ -e "${TARGET_DIR}" ]
     then
         pushd $TARGET_DIR
-        sudo -u $SUDO_USER git fetch
-        exists=$(sudo -u $SUDO_USER git show-branch $BRANCH > /dev/null 2>&1; echo $?)
+        sudo -u $username git fetch
+        exists=$(sudo -u $username git show-branch $BRANCH > /dev/null 2>&1; echo $?)
         if [ "$exists" == "0" ]
         then
             echo "$TARGET_DIR already up to date."
         else
-            sudo -u $SUDO_USER git checkout -b $BRANCH origin/$BRANCH
+            sudo -u $username git checkout -b $BRANCH origin/$BRANCH
         fi
         popd_if_stack_not_empty
     else
-        sudo -u $SUDO_USER git clone $REPO $TARGET_DIR
+        sudo -u $username git clone $REPO $TARGET_DIR
     fi
     popd_if_stack_not_empty
 
     #################################
     # Clone zsh syntax highlighting #
     #################################
-    TARGET_DIR="${ZSH_CUSTOM:-$sudo_user_home/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    TARGET_DIR="${ZSH_CUSTOM:-$user_home/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
     REPO="https://github.com/zsh-users/zsh-syntax-highlighting.git"
-    BRANCH=$(sudo -u $SUDO_USER git ls-remote --heads $REPO | grep -q 'refs/heads/main' && echo 'main' || (sudo -u $SUDO_USER git ls-remote --heads $REPO | grep -q 'refs/heads/master' && echo 'master') || echo null)
+    BRANCH=$(sudo -u $username git ls-remote --heads $REPO | grep -q 'refs/heads/main' && echo 'main' || (sudo -u $username git ls-remote --heads $REPO | grep -q 'refs/heads/master' && echo 'master') || echo null)
 
     if [ -e "${TARGET_DIR}" ]
     then
         pushd $TARGET_DIR
-        sudo -u $SUDO_USER git fetch
-        exists=$(sudo -u $SUDO_USER git show-branch $BRANCH > /dev/null 2>&1; echo $?)
+        sudo -u $username git fetch
+        exists=$(sudo -u $username git show-branch $BRANCH > /dev/null 2>&1; echo $?)
         if [ "$exists" == "0" ]
         then
             echo "$TARGET_DIR already up to date."
         else
-            sudo -u $SUDO_USER git checkout -b $BRANCH origin/$BRANCH
+            sudo -u $username git checkout -b $BRANCH origin/$BRANCH
         fi
         popd_if_stack_not_empty
     else
-        sudo -u $SUDO_USER git clone $REPO $TARGET_DIR
+        sudo -u $username git clone $REPO $TARGET_DIR
     fi
     popd_if_stack_not_empty
 
     ###########################
     # Clone zsh auto complete #
     ###########################
-    TARGET_DIR="${ZSH_CUSTOM:-$sudo_user_home/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+    TARGET_DIR="${ZSH_CUSTOM:-$user_home/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
     REPO="https://github.com/zsh-users/zsh-autosuggestions"
-    BRANCH=$(sudo -u $SUDO_USER git ls-remote --heads $REPO | grep -q 'refs/heads/main' && echo 'main' || (sudo -u $SUDO_USER git ls-remote --heads $REPO | grep -q 'refs/heads/master' && echo 'master') || echo null)
+    BRANCH=$(sudo -u $username git ls-remote --heads $REPO | grep -q 'refs/heads/main' && echo 'main' || (sudo -u $username git ls-remote --heads $REPO | grep -q 'refs/heads/master' && echo 'master') || echo null)
 
     if [ -e "${TARGET_DIR}" ]
     then
         pushd $TARGET_DIR
-        sudo -u $SUDO_USER git fetch
-        exists=$(sudo -u $SUDO_USER git show-branch $BRANCH > /dev/null 2>&1; echo $?)
+        sudo -u $username git fetch
+        exists=$(sudo -u $username git show-branch $BRANCH > /dev/null 2>&1; echo $?)
         if [ "$exists" == "0" ]
         then
             echo "$TARGET_DIR already up to date."
         else
-            sudo -u $SUDO_USER git checkout -b $BRANCH origin/$BRANCH
+            sudo -u $username git checkout -b $BRANCH origin/$BRANCH
         fi
         popd_if_stack_not_empty
     else
-        sudo -u $SUDO_USER git clone $REPO $TARGET_DIR
+        sudo -u $username git clone $REPO $TARGET_DIR
     fi
     popd_if_stack_not_empty
 fi
@@ -298,13 +298,13 @@ fi
 customFontName="Agave"
 customFontFile="$customFontName.zip"
 fontURL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/$customFontName.zip"
-fontDir="${sudo_user_home}/.local/share/fonts/$customFontName"
+fontDir="${user_home}/.local/share/fonts/$customFontName"
 
 if [[ ! -d $fontDir ]]; then
-    sudo -u $SUDO_USER curl -L -O $fontURL
-    sudo -u $SUDO_USER mkdir -p $fontDir
-    sudo -u $SUDO_USER unzip $customFontFile -d $fontDir
-    sudo -u $SUDO_USER rm $customFontFile
+    sudo -u $username curl -L -O $fontURL
+    sudo -u $username mkdir -p $fontDir
+    sudo -u $username unzip $customFontFile -d $fontDir
+    sudo -u $username rm $customFontFile
 else
     echo "Font '$customFontName' already installed - skipping"
 fi
@@ -321,8 +321,8 @@ configDownloadList=(
 
 for cloudFile in "${configDownloadList[@]}"; do 
     fileName=$(basename "$cloudFile")
-    localFilePath="${sudo_user_home}/${fileName}"
-    backupDir="${sudo_user_home}/config_backups"
+    localFilePath="${user_home}/${fileName}"
+    backupDir="${user_home}/config_backups"
     backupCount=5
 
     # Check if local file and remote file have the same checksum
@@ -331,13 +331,13 @@ for cloudFile in "${configDownloadList[@]}"; do
     else
         # Create backup directory if it doesn't exist
         if [[ ! -d "$backupDir" ]]; then
-            sudo -u $SUDO_USER mkdir "$backupDir"
+            sudo -u $username mkdir "$backupDir"
         fi
 
         # Backup existing file with date timestamp
         timestamp=$(date +%Y%m%d%H%M%S)
         backupFilePath="${backupDir}/${fileName}.${timestamp}"
-        sudo -u $SUDO_USER cp "$localFilePath" "$backupFilePath"
+        sudo -u $username cp "$localFilePath" "$backupFilePath"
 
         echo "Existing $fileName backed up to $backupFilePath"
 
@@ -352,11 +352,11 @@ for cloudFile in "${configDownloadList[@]}"; do
 
             # Remove oldest backups
             for ((i = 0; i < backupCount; i++)); do
-                sudo -u $SUDO_USER rm "${sortedBackups[$i]}"
+                sudo -u $username rm "${sortedBackups[$i]}"
                 echo "Removed old backup: ${sortedBackups[$i]}"
             done
         fi
         echo "Downloading $fileName..."
-        sudo -u $SUDO_USER curl -o "$localFilePath" "$cloudFile"
+        sudo -u $username curl -o "$localFilePath" "$cloudFile"
     fi
 done
